5a7f11d45cf7f5144b7ab8ee22a548b964ba7006
Tiago <tiagosantiniathayde@gmail.com>
2022-06-15 14:11:34 -0300

    Implementado verificação de opções do menu

--- CalculadoraTabajara.ConsoleApp/Program.cs
+++ CalculadoraTabajara.ConsoleApp/Program.cs
@@ -37,6 +37,13 @@ namespace CalculadoraTabajara.ConsoleApp
                 if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
                     break;
 
+                if (opcao != "1" && opcao != "2" && opcao != "3")
+                {
+                    Console.WriteLine("Por favor, digite uma opção válida!");
+                    Console.ReadLine();
+                    continue;
+                }
+
                 #endregion
 
                 #region Realizar Calculo
